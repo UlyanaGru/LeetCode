9ac057e04fad6ae7ffd670baa4e000f305da046a
UlyanaGru <giperbolicheskiisinys@gmail.com>
2026-01-25 18:32:30 +0300

    hashmap on java 118 / 169 testcases passed

---
+++ Leetcode.StudyPlan/javamodetasks.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class javamodetasks {
+    public boolean closeStrings(String word1, String word2){
+        HashMap<Character,Integer> quant1 = new HashMap<>();
+        HashMap<Character,Integer> quant2 = new HashMap<>();
+        Integer len1 = word1.length();
+        Integer len2 = word2.length();
+        if (len1 != len2){
+            return false;
+        }
+        for (char ch1: word1.toCharArray()){
+            quant1.put(ch1,quant1.getOrDefault(ch1,0)+1);
+        }
+        for (char ch2: word2.toCharArray()){
+            quant2.put(ch2,quant2.getOrDefault(ch2,0)+1);
+        }
+        if (!(quant1.keySet()).equals(quant2.keySet())){
+            return false;
+        }
+        List<Integer> freq1 = new ArrayList<>(quant1.values());
+        List<Integer> freq2 = new ArrayList<>(quant2.values());
+        Collections.sort(freq1);
+        Collections.sort(freq2);
+        
+        return freq1.equals(freq2);
+
+    }
+    public static void main(String[] args){
+        javamodetasks sol = new javamodetasks();
+        System.out.println(sol.closeStrings("aaabbbc", "bcccaaa"));
+        System.out.println(sol.closeStrings("aa", "a"));
+    }
+}
