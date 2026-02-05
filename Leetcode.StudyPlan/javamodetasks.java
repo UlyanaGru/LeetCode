e3c24462c13a76739201a951529544b86d8817c4
UlyanaGru <giperbolicheskiisinys@gmail.com>
2026-02-05 14:04:47 +0300

    206 ms Beats 16.73%

--- Leetcode.StudyPlan/javamodetasks.java
+++ Leetcode.StudyPlan/javamodetasks.java
@@ -1,35 +1,25 @@
 import java.util.*;
 
 public class javamodetasks {
-    public boolean closeStrings(String word1, String word2){
-        HashMap<Character,Integer> quant1 = new HashMap<>();
-        HashMap<Character,Integer> quant2 = new HashMap<>();
-        int len1 = word1.length(); //with Integer problems with length, on contrary with int all's ok
-        int len2 = word2.length();
-        if (len1 != len2){
-            return false;
+
+    public static String RemoveStrings(String s){
+        ArrayList<String> stack = new ArrayList<>();
+        String[] subStrings = s.split("");
+        for (String ch: subStrings){
+            int size = stack.size();
+            if (ch.equals("*") && size > 0){
+                int index = size - 1;
+                stack.remove(index);
+            }
+            else{
+                stack.add(ch);
+            }
         }
-        for (char ch1: word1.toCharArray()){
-            quant1.put(ch1,quant1.getOrDefault(ch1,0)+1);
+        return String.join("",stack);
         }
-        for (char ch2: word2.toCharArray()){
-            quant2.put(ch2,quant2.getOrDefault(ch2,0)+1);
+    public static void main(String[] args){
+        String answ = javamodetasks.RemoveStrings("leet**code");
+        System.out.println(answ);
         }
-        if (!(quant1.keySet()).equals(quant2.keySet())){
-            return false;
-        }
-        List<Integer> freq1 = new ArrayList<>(quant1.values());
-        List<Integer> freq2 = new ArrayList<>(quant2.values());
-        Collections.sort(freq1);
-        Collections.sort(freq2);
-        
-        return freq1.equals(freq2);
 
     }
-    public static void main(String[] args){
-        javamodetasks sol = new javamodetasks();
-        System.out.println(sol.closeStrings("aaabbbc", "bcccaaa"));
-        System.out.println(sol.closeStrings("aa", "a"));
-        System.out.println(sol.closeStrings("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"));
-    }
-}
